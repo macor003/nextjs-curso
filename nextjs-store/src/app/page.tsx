e4855b6285f87bf58b6d766f5356820a1c3dd93a
Mario Ortega <mcor63@gmail.com>
2024-08-25 09:58:05 -0500

    Se agregan ejercicios de practica.

--- nextjs-store/src/app/page.tsx
+++ nextjs-store/src/app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
 import styles from "./page.module.css";
 
+
 export default function Home() {
-  return (
+  console.log("Hola Mundo!");
+  return (    
     <main className={styles.main}>
       <h1>Hola Mundo!</h1>
     </main>
